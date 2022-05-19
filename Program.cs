3f3c83f23ab4f980b7c9f4c1923ca644213a7204
Carlos Eduardo <65124135+KingTeox@users.noreply.github.com>
2022-05-19 18:07:05 -0300

    </> NODEMON </>

--- Program.cs
+++ Program.cs
@@ -1,37 +1,46 @@
 ﻿using System.Diagnostics;
 
+#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+
 namespace Program
 {
     class Load
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string strCmdText;
             bool Exist = File.Exists("index.js");
             Console.Title = "Multi Loader...";
             if (Exist) {
                 Console.Write("Atualizar pacotes ?\n");
                 Console.Write("Digite: ");
-#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
                 string Valor = Console.ReadLine();
-#pragma warning restore CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
 
-                if (Valor == "" || Valor == null || Valor == "null") { Valor = "Nada"; };
+
+                if (Valor == "" || Valor == null || Valor == "null") { Valor = "Nao"; };
 
                 Console.Clear();
 
+                Console.Write("Iniciar com nodemon ?\n");
+                Console.Write("Digite: ");
+
+                string nodemon = Console.ReadLine();
+                string starter = "node index.js && pause";
+
+                Console.Clear();
+
+                if (nodemon == "" || nodemon == null || nodemon == "null") { nodemon = "Nao"; };
+                if (nodemon.ToLower() == "sim" || nodemon.ToLower() == "yes")
+                {
+                    starter = "nodemon index.js && pause";
+                };
+
                 if (Valor.ToLower() == "sim" || Valor.ToLower() == "yes")
                 {
-                    strCmdText = "'/C npm update --save && node index.js";
-                    Process.Start("CMD.exe", strCmdText);
-                } else
-                {
-                    strCmdText = "'/C node index.js";
-                    Process.Start("CMD.exe", strCmdText);
-                };
+                    Process.Start("CMD.exe", "'/C npm update --save && " + starter);
+                    return;
+                };  Process.Start("CMD.exe", "'/C " + starter);
             } else {
-                strCmdText = "'/C echo Arquivo index.js nao existe && pause";
-                Process.Start("CMD.exe", strCmdText);
+                Process.Start("CMD.exe", "'/C echo Arquivo index.js nao existe && pause");
             };
         }
     }
